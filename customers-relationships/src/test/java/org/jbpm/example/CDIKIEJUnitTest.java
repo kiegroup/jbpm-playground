fc7e2f7c9f59ebd419ef0bde43d19c7d1be1126c
salaboy <salaboy@gmail.com>
2013-10-21 16:13:47 +0100

    - adding new cdi and non cdi tests for the customer services project

---
+++ customers-relationships/src/test/java/org/jbpm/example/CDIKIEJUnitTest.java
@@ -0,0 +1,159 @@
+package org.jbpm.example;
+
+import bitronix.tm.resource.jdbc.PoolingDataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jbpm.customer.services.AddCustomerCommentsWorkItemHandler;
+import org.jbpm.customer.services.CreateCustomerWorkItemHandler;
+import org.jbpm.customer.services.ManagersReportWorkItemHandler;
+import org.jbpm.services.task.utils.ContentMarshallerHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.Content;
+import org.kie.api.task.model.Task;
+import org.kie.api.task.model.TaskSummary;
+import org.kie.internal.runtime.manager.cdi.qualifier.Singleton;
+import org.kie.internal.runtime.manager.context.EmptyContext;
+
+/**
+ *
+ * @author salaboy
+ */
+@RunWith(Arquillian.class)
+public class CDIKIEJUnitTest  {
+
+    public CDIKIEJUnitTest() {
+    }
+
+    private static PoolingDataSource pds;
+
+    @Deployment()
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "jbpm-cdi-sample.jar")
+                .addPackage("org.jboss.seam.transaction")
+                .addPackages(true, "org.kie.api.runtime.manager")
+                .addPackages(true, "org.kie.internal.runtime.manager")
+                .addPackages(true, "org.jbpm.kie")
+                .addPackages(true, "org.jbpm.kie.services")
+                .addPackages(true, "org.jbpm.services")
+                .addPackages(true, "org.jbpm.services.task")
+                .addPackages(true, "org.jbpm.services.task.identity")
+                .addPackages(true, "org.jbpm.runtime")
+                .addPackages(true, "org.jbpm.runtime.manager.impl")
+                .addPackages(true, "org.jbpm.shared")
+                .addPackage("org.jbpm.example")
+                .addAsManifestResource("META-INF/persistence.xml", ArchivePaths.create("persistence.xml"))
+                .addAsManifestResource("META-INF/beans.xml", ArchivePaths.create("beans.xml"));
+   
+    }
+    
+    
+    @BeforeClass
+    public static void setupOnce() {
+        pds = new PoolingDataSource();
+        pds.setUniqueName("jdbc/jbpm-ds");
+        pds.setClassName("bitronix.tm.resource.jdbc.lrc.LrcXADataSource");
+        pds.setMaxPoolSize(5);
+        pds.setAllowLocalTransactions(true);
+        pds.getDriverProperties().put("user", "sa");
+        pds.getDriverProperties().put("password", "");
+        pds.getDriverProperties().put("url", "jdbc:h2:mem:jbpm-db;MVCC=true");
+        pds.getDriverProperties().put("driverClassName", "org.h2.Driver");
+        pds.init();
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        if (pds != null) {
+            pds.close();
+        }
+    }
+
+    @Inject
+    @Singleton
+    @ApplicationScoped
+    private RuntimeManager runtimeManager;
+
+  
+    @Test
+    public void processExecutionTest() {
+        
+        RuntimeEngine engine = runtimeManager.getRuntimeEngine(EmptyContext.get());
+        assertNotNull(engine);
+        KieSession kieSession = engine.getKieSession();
+        
+        kieSession.getWorkItemManager().registerWorkItemHandler("CreateCustomer", new CreateCustomerWorkItemHandler());
+        kieSession.getWorkItemManager().registerWorkItemHandler("AddCustomerComment", new AddCustomerCommentsWorkItemHandler());
+        kieSession.getWorkItemManager().registerWorkItemHandler("ManagersReport", new ManagersReportWorkItemHandler());
+        
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("customer_phone", "4444-55555");
+        ProcessInstance processInstance = kieSession.startProcess("CustomersRelationship.customers", params);
+        
+        assertNotNull(processInstance);
+        TaskService taskService = engine.getTaskService();
+        List<TaskSummary> tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner("mary", "en-UK");
+        
+        assertTrue(!tasksAssignedAsPotentialOwner.isEmpty());
+        TaskSummary taskSummary = tasksAssignedAsPotentialOwner.get(0);
+        assertEquals("Obtain Customer Info", taskSummary.getName());
+        
+        taskService.claim(taskSummary.getId(), "mary");
+        
+        taskService.start(taskSummary.getId(), "mary");
+
+        Task obtainDataTask = taskService.getTaskById(taskSummary.getId());
+
+        long contentId = obtainDataTask.getTaskData().getDocumentContentId();
+        assertTrue(contentId != -1);
+
+        Content content = taskService.getContentById(contentId);
+        Object unmarshalledObject = ContentMarshallerHelper.unmarshall(content.getContent(), null);
+        if (!(unmarshalledObject instanceof Map)) {
+            fail("The variables should be a Map");
+
+        }
+        Map<String, Object> unmarshalledvars = (Map<String, Object>) unmarshalledObject;
+
+        assertEquals("4444-55555", unmarshalledvars.get("in_customer_phone"));
+
+        Map<String, Object> taskParams = new HashMap<String, Object>();
+        taskParams.put("out_customer_name", "salaboy");
+        taskParams.put("out_customer_lastname", "salaboyLN");
+        taskParams.put("out_customer_age", "30");
+        taskParams.put("out_customer_email", "fake@email.com");
+        taskParams.put("out_customer_address", "fake address 45");
+
+        taskService.complete(taskSummary.getId(), "mary", taskParams);
+        
+        tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner("mary", "en-UK");
+        assertTrue(!tasksAssignedAsPotentialOwner.isEmpty());
+        
+        taskSummary = tasksAssignedAsPotentialOwner.get(0);
+        assertEquals("Call Customer For Comments", taskSummary.getName());
+        
+        taskService.claim(taskSummary.getId(), "mary");
+        
+        //.. continue with the next task from here
+        
+    }
+}
---
+++ customers-relationships/src/test/java/org/jbpm/example/SimpleProcessJUnitTest.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jbpm.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.jbpm.customer.services.AddCustomerCommentsWorkItemHandler;
+import org.jbpm.customer.services.CreateCustomerWorkItemHandler;
+import org.jbpm.customer.services.ManagersReportWorkItemHandler;
+import org.jbpm.services.task.utils.ContentMarshallerHelper;
+import org.jbpm.test.JbpmJUnitBaseTestCase;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.Content;
+import org.kie.api.task.model.Task;
+import org.kie.api.task.model.TaskSummary;
+
+/**
+ *
+ * @author salaboy
+ */
+public class SimpleProcessJUnitTest extends JbpmJUnitBaseTestCase {
+
+    public SimpleProcessJUnitTest() {
+        super(true, true, "org.jbpm.sample");
+    }
+
+    @Test
+    public void simpleProcessTest() {
+        Map<String, ResourceType> resources = new HashMap<String, ResourceType>();
+        resources.put("customers.bpmn2", ResourceType.BPMN2);
+        createRuntimeManager(resources, "simple-no-cdi-test");
+        RuntimeEngine engine = getRuntimeEngine();
+        assertNotNull(engine);
+
+        KieSession ksession = engine.getKieSession();
+        ksession.getWorkItemManager().registerWorkItemHandler("CreateCustomer", new CreateCustomerWorkItemHandler());
+        ksession.getWorkItemManager().registerWorkItemHandler("AddCustomerComment", new AddCustomerCommentsWorkItemHandler());
+        ksession.getWorkItemManager().registerWorkItemHandler("ManagersReport", new ManagersReportWorkItemHandler());
+        
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("customer_phone", "4444-55555");
+        ProcessInstance processInstance = ksession.startProcess("CustomersRelationship.customers", params);
+
+        assertNotNull(processInstance);
+
+        TaskService taskService = engine.getTaskService();
+        List<TaskSummary> tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner("mary", "en-UK");
+
+        assertTrue(!tasksAssignedAsPotentialOwner.isEmpty());
+        TaskSummary taskSummary = tasksAssignedAsPotentialOwner.get(0);
+        assertEquals("Obtain Customer Info", taskSummary.getName());
+        
+        taskService.claim(taskSummary.getId(), "mary");
+
+        taskService.start(taskSummary.getId(), "mary");
+
+        Task obtainDataTask = taskService.getTaskById(taskSummary.getId());
+
+        long contentId = obtainDataTask.getTaskData().getDocumentContentId();
+        assertTrue(contentId != -1);
+
+        Content content = taskService.getContentById(contentId);
+        Object unmarshalledObject = ContentMarshallerHelper.unmarshall(content.getContent(), null);
+        if (!(unmarshalledObject instanceof Map)) {
+            fail("The variables should be a Map");
+
+        }
+        Map<String, Object> unmarshalledvars = (Map<String, Object>) unmarshalledObject;
+
+        assertEquals("4444-55555", unmarshalledvars.get("in_customer_phone"));
+
+        Map<String, Object> taskParams = new HashMap<String, Object>();
+        taskParams.put("out_customer_name", "salaboy");
+        taskParams.put("out_customer_lastname", "salaboyLN");
+        taskParams.put("out_customer_age", "30");
+        taskParams.put("out_customer_email", "fake@email.com");
+        taskParams.put("out_customer_address", "fake address 45");
+
+        taskService.complete(taskSummary.getId(), "mary", taskParams);
+        
+        tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner("mary", "en-UK");
+        assertTrue(!tasksAssignedAsPotentialOwner.isEmpty());
+        
+        taskSummary = tasksAssignedAsPotentialOwner.get(0);
+        assertEquals("Call Customer For Comments", taskSummary.getName());
+        
+        taskService.claim(taskSummary.getId(), "mary");
+        
+        //.. continue with the next task from here
+
+    }
+
+}
