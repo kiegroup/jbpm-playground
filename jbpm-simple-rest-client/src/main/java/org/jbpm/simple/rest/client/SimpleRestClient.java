4969efa6375852718dc4e408532127000f2aa73a
Salaboy <salaboy@gmail.com>
2014-03-07 10:19:00 +0000

    - adding short demo of auditServices

--- jbpm-simple-rest-client/src/main/java/org/jbpm/simple/rest/client/SimpleRestClient.java
+++ jbpm-simple-rest-client/src/main/java/org/jbpm/simple/rest/client/SimpleRestClient.java
@@ -3,12 +3,16 @@ package org.jbpm.simple.rest.client;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.jbpm.process.audit.AuditLogService;
+import org.jbpm.process.audit.ProcessInstanceLog;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.task.TaskService;
 import org.kie.services.client.api.RemoteRestRuntimeFactory;
+import org.kie.services.client.api.command.RemoteRuntimeEngine;
 
 /**
  * This is a very simple Rest Client to test against a running
@@ -27,12 +31,14 @@ public class SimpleRestClient
         String user = "jbpm";
         String password = "jbpm6";
         
+        
         RemoteRestRuntimeFactory restSessionFactory 
             = new RemoteRestRuntimeFactory(deploymentId, appUrl, user, password);
-        RuntimeEngine engine = restSessionFactory.newRuntimeEngine();
+        RemoteRuntimeEngine engine = restSessionFactory.newRuntimeEngine();
         
         KieSession ksession = engine.getKieSession();
         TaskService taskService = engine.getTaskService();
+        AuditLogService auditLogService = engine.getAuditLogService();
         
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("candidate", "johny");
@@ -40,9 +46,12 @@ public class SimpleRestClient
         ProcessInstance processInstance = ksession.startProcess("hiring", params);
         // do some more interactions here... for example try the Human Task APIs
         // List<TaskSummary> tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner("jbpm", "en-UK"); 
-//        Collection<ProcessInstance> processInstances = ksession.getProcessInstances();
-//        for(ProcessInstance pi : processInstances){
-//            System.out.println("Process Instance: "+pi.getProcessId() + pi.getProcessName());
-//        }
+        List<ProcessInstanceLog> findProcessInstances = auditLogService.findProcessInstances();
+        for(ProcessInstanceLog pi : findProcessInstances){
+            System.out.println("Process Instance: "+pi.getProcessName() + " - Version: "+pi.getProcessVersion() + " - Started at: "+pi.getStart());
+        }
+        
+        
+        
     }   
 }
