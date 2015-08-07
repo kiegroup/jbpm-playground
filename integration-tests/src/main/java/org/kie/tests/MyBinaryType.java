6c4080f09b4de7a26b19ee4bc523e3a92ab21ff0
Marco Rietveld <mrietvel@redhat.com>
2015-08-07 11:04:48 +0200

    integration-tests: added MyBinaryType to test file attachment process variables

---
+++ integration-tests/src/main/java/org/kie/tests/MyBinaryType.java
@@ -0,0 +1,46 @@
+package org.kie.tests;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+
+@XmlRootElement(name="my-type")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class MyBinaryType {
+
+    @XmlElement
+    @XmlSchemaType(name="string")
+    private String name;
+    
+    @XmlElement
+    @XmlSchemaType(name="base64Binary")
+    private byte [] data;
+    
+    public MyBinaryType() {
+       // default constructor 
+    }
+    
+    public MyBinaryType(String name, byte [] data) {
+        this.name = name;
+        this.data = data;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte [] getData() {
+        return data;
+    }
+    
+    public void setData(byte [] data) {
+        this.data = data;
+    }
+
+}
